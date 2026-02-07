bb1dd25ae3ad1e17acfb03e0211386308106ec7e
ZoomInside <YanislavBaev@outlook.com>
2026-02-07 11:55:02 +0200

    InitialCommit

---
+++ Курсов проект - ИтКариера/Курсов проект - ИтКариера/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Курсов_проект___ИтКариера.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
