5ab6e26a765196e3805e371b6f9a378ed3d2eb5a
adrianngg <adrianganchev28@gmail.com>
2026-04-04 19:47:54 +0300

    Added-new-lists-functionality

---
+++ Курсов проект - ИтКариера/Курсов проект - ИтКариера/Data/ReadingList.cs
@@ -0,0 +1,11 @@
+﻿namespace Курсов_проект___ИтКариера.Data
+{
+    public class ReadingList
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<ReadingListBook> ReadingListBooks { get; set; } = new List<ReadingListBook>();
+    }
+}
---
+++ Курсов проект - ИтКариера/Курсов проект - ИтКариера/Data/ReadingListBook.cs
@@ -0,0 +1,10 @@
+﻿namespace Курсов_проект___ИтКариера.Data
+{
+    public class ReadingListBook
+    {
+        public Guid ReadingListId { get; set; }
+        public ReadingList ReadingList { get; set; }
+        public Guid BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
